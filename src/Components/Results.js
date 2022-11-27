6d9ca8744ddb0f6ee111a192f39de00a3930e64f
Rene De La Calleja <renedlc1@gmail.com>
2022-11-27 14:23:32 -0600

    Api cleanup

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -2,50 +2,23 @@ import { useState } from "react";
 import React from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs} from 'firebase/firestore/lite';
+import firestoreApi from '../API/firestoreApi'
 
 const Results = () => {
 
   const [DBResults, setDBResults] = useState([]);
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAkBkc4-tYi52B9Y4HU9JQ0bixY7-b28bQ",
-    authDomain: "recipe-finder-5f0aa.firebaseapp.com",
-    databaseURL: "https://recipe-finder-5f0aa-default-rtdb.firebaseio.com/",
-    projectId: "recipe-finder-5f0aa",
-    storageBucket: "recipe-finder-5f0aa.appspot.com",
-    messagingSenderId: "211665410735",
-    appId: "1:211665410735:web:b597cf786acc9395b5925a",
-    measurementId: "G-V8Y4CFERW0"
-  };
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+  // firestoreApi.getDocs().then((response)=>{console.log('Response',response.docs[0].data());})
 
   React.useEffect(()=>{
-    var newState = [];
-    async function readDB() {
-      const recipieCllection = collection(db, 'results');
-      const recipieSnapshot = await getDocs(recipieCllection);
-      const recipeList = recipieSnapshot.docs.map(doc => doc.data());
-      
-      for(let i = 0; i < recipeList.length; i++){
-        newState.push({
-          id: recipeList[i].id,
-          calories: recipeList[i].calories,
-          carbohydrates: recipeList[i].carbohydrates,
-          dat: recipeList[i].day,
-          fats: recipeList[i].fats,
-          proteins: recipeList[i].proteins,
-          reminder: recipeList[i].reminder,
-          text: recipeList[i].text,
-        })
-        
-      }
-    };
-    readDB();
-    setDBResults(newState);
+    firestoreApi.getDocs().then((response)=>{
+     const parsedArray = response.docs.map((element)=>element.data());
+      setDBResults(parsedArray);
+    });
+    // setDBResults(newState);
     //console.log(newState);
   }, []);
-  console.log(DBResults);
+  console.log('State', DBResults);
   return (
     <div>
     <h1>Results</h1>
